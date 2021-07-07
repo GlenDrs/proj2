19d87824f6bcf96724ab00538e3f2585643b1e15
Glen <67809619+GlenDu@users.noreply.github.com>
2021-07-07 18:45:11 +0200

    Update exc12.rb

--- exc12.rb
+++ exc12.rb
@@ -1,34 +1,12 @@
 /2.12 Annoncer l'âge, option BG/
 
 
-puts("Quelle age a tu coco? ")
-age2=gets.to_i
- anNais2=2021-age2
- moit=age/2  
- 
-
- for i in 0..age2
-    
- when:i..moit do puts ("il y a #{age2 } ans t'a eu #{i} ans coco")
- end
- when:moit do puts ("il y a #{i} ans tu avais la moitié de l'age que t'a aujourd'hui mon vieu!!! ")
- end
- when:moit..age2 do puts ("il y a #{age2 } ans t'a eu #{i} ans coco")
- end
- 
- 
- p ("!!!")
-end
-
-
-Or 
-
-loop do 
-    i=i+1
-    age=age-1
-    puts ("il y a #{age2 } ans t'a eu #{i} ans coco")
-    if i==age2
-        puts ("la tu avait la moitié de ton age: ")
+puts "quel est votre année de naissance?"
+nombre = gets.chomp.to_i
+for i in nombre..2021
+    if 2021-i ==i-nombre
+        puts "Il y a #{2021-i} tu avais la moitié de l'âge que tu as aujourd'hui"
+    else
+        puts "Il y a #{2021-i} tu avais #{i-nombre}"
     end
-
 end
